991beac443bbf498874959c364565c59a2acb4b3
Lin Khant Htel <linkhanthtel@icloud.com>
2024-07-17 00:36:44 +0800

    17 Jul Commit

--- src/pages/shop.jsx
+++ src/pages/shop.jsx
@@ -12,7 +12,7 @@ export const Shop = () => {
                 </div>
                 <div className="flex flex-col items-center text-xl pt-20 px-10">
                     <p className="font-light">Reward yourself with amazing items and products from LKH store. Taste the best online shopping experience with awesome items with reasonable price. Claim up to 50% with LKH gift cards.</p>
-                    <button onClick={() => {alert("You have claimed your discount!")}} className="p-3 my-5 font-extralight text-white bg-green-700 rounded-full animate-bounce">Claim Discount</button>
+                    <button onClick={() => {alert("You have claimed your discount!")}} className="p-3 mt-16 font-extralight text-white bg-green-700 rounded-full animate-bounce">Claim Discount</button>
                 </div>
             </div>
 
