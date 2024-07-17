489d182e2e536800f41d329d0080a4b63360d2cb
Lin Khant Htel <linkhanthtel@icloud.com>
2024-07-18 00:19:18 +0800

    Shop Page Updated

--- src/pages/shop.jsx
+++ src/pages/shop.jsx
@@ -6,11 +6,12 @@ export const Shop = () => {
     return(
         <div>
             {/* Hero Section */}
-            <div className="hidden md:grid grid-cols-1 md:grid-cols-2 h-[400px] bg-white">
-                <div className="flex justify-center items-center">
+            <div className="grid grid-cols-1 md:grid-cols-2 h-[450px] bg-white">
+                <div className="flex flex-col justify-center items-center">
                     <img src={heroimage} alt="Image" className="h-72" />
+                    <p className="flex text-center md:hidden">Reward yourself with amazing items and products from LKH store. Taste the best online shopping experience with awesome items with reasonable price.</p>
                 </div>
-                <div className="flex flex-col items-center text-xl pt-20 px-10">
+                <div className="hidden md:flex flex-col items-center text-xl pt-20 px-10">
                     <p className="font-light">Reward yourself with amazing items and products from LKH store. Taste the best online shopping experience with awesome items with reasonable price. Claim up to 50% with LKH gift cards.</p>
                     <button onClick={() => {alert("You have claimed your discount!")}} className="p-3 mt-16 font-extralight text-white bg-green-700 rounded-full animate-bounce">Claim Discount</button>
                 </div>
