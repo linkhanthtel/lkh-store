5e21c7b6b982e766fdc8ee0bef57885540feee23
Lin Khant Htel <linkhanthtel@icloud.com>
2024-07-22 23:36:09 +0800

    Logo and minor UI Updated

--- src/pages/shop.jsx
+++ src/pages/shop.jsx
@@ -10,6 +10,7 @@ export const Shop = () => {
                 <div className="flex flex-col justify-center items-center">
                     <img src={heroimage} alt="Image" className="h-72" />
                     <p className="flex text-center md:hidden">Reward yourself with amazing items and products from LKH store. Taste the best online shopping experience with awesome items with reasonable price.</p>
+                    <button onClick={() => {alert("You have claimed your discount!")}} className="p-3 mt-5 flex md:hidden font-extralight text-white bg-green-700 rounded-full animate-bounce">Claim Discount</button>
                 </div>
                 <div className="hidden md:flex flex-col items-center text-xl pt-20 px-10">
                     <p className="font-light">Reward yourself with amazing items and products from LKH store. Taste the best online shopping experience with awesome items with reasonable price. Claim up to 50% with LKH gift cards.</p>
