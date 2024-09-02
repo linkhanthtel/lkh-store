ce636304a3175a4100d30fb01b1457ab219f14c8
Lin Khant Htel <linkhanthtel@icloud.com>
2024-09-02 23:52:56 +0800

    Home Page updated

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <div className='mt-60 p-7 bg-blue-950 text-white text-center'>
+    <div className='p-7 bg-blue-950 text-white text-center'>
       <h1>©️ Copyright 2024 | LKH Store | All rights reserved</h1>
     </div>
   )
